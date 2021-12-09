88865405b6dd3d2685b4530a1d9d59ee99a676e4
NamelessHoodie <73077577+NamelessHoodie@users.noreply.github.com>
2021-12-08 21:25:16 -0800

    actually made the extension exist in this repository

---
+++ HoodieScript-VSCode/main.js
@@ -0,0 +1,3 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = () => { console.log("Initialized HoodieScript-VSCode successfully"); };
+exports.deactivate = () => { };
