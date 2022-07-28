98ab3056efb879061af5941231684d12c4ebd4f3
NamelessHoodie <namelesshoodie@gmail.com>
2022-07-28 09:55:20 -0700

    forgor one definition in entry.h, should be it now.

--- HoodieScript/entry.h
+++ HoodieScript/entry.h
@@ -1,8 +1,10 @@
 #pragma once
 
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved);
+DWORD WINAPI init_thread(void* lpParam);
+
 void attach();
 void detach();
 void prepare_console();
 void free_console();
 
-DWORD WINAPI init_thread(void* lpParam);
